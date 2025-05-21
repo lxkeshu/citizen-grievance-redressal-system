05ea4c884165d1d819e270f3442e8c8f91379e84
Lokesh Agarwal <lxkeshu@gmail.com>
2025-05-21 12:57:43 +0530

    refactor: move firebase config to root directory

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+// Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyBdm8dyQeVQ3NBdqGSDnPbz3UZ2SKM9pNk",
+  authDomain: "citizengrievance-757e5.firebaseapp.com",
+  projectId: "citizengrievance-757e5",
+  storageBucket: "citizengrievance-757e5.firebasestorage.app",
+  messagingSenderId: "765489875675",
+  appId: "1:765489875675:web:ddfd1f95a5a09062badafe",
+  measurementId: "G-2SKFV26SRP"
+}; 
--- src/firebase.js
+++ src/firebase.js
@@ -3,17 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBdm8dyQeVQ3NBdqGSDnPbz3UZ2SKM9pNk",
-  authDomain: "citizengrievance-757e5.firebaseapp.com",
-  projectId: "citizengrievance-757e5",
-  storageBucket: "citizengrievance-757e5.firebasestorage.app",
-  messagingSenderId: "765489875675",
-  appId: "1:765489875675:web:ddfd1f95a5a09062badafe",
-  measurementId: "G-2SKFV26SRP"
-};
+import { firebaseConfig } from '../firebase.config.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
