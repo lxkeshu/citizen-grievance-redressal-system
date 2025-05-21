758733e19e232808d937c19f59856cef5bba3545
Lokesh Agarwal <lxkeshu@gmail.com>
2025-05-21 12:50:12 +0530

    fix: update firebase config import path to use absolute path

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { firebaseConfig } from './firebase.config';
+import { firebaseConfig } from '/src/firebase.config.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
