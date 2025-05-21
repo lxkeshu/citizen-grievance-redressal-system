2d798d67e93d8d889daef4bcbfb983a294084219
Lokesh Agarwal <lxkeshu@gmail.com>
2025-05-21 12:28:02 +0530

    fix: remove .js extension from firebase config import

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { firebaseConfig } from './firebase.config.js';
+import { firebaseConfig } from './firebase.config';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
