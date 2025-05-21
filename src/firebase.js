1ecc2fdcfae9d95e079b66a4bf1b533bc98bcdf8
Lokesh Agarwal <lxkeshu@gmail.com>
2025-05-21 12:13:03 +0530

    fix: update firebase config import path

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,12 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { firebaseConfig } from './firebase.config';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { firebaseConfig } from './firebase.config.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app); 
+export const db = getFirestore(app);
+export const storage = getStorage(app); 
